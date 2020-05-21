b718f5325ef8dce7637c5bfc5b69295d5cc9ec5e
john.goldthorpe <john.goldthorpe@balfourbeatty.com>
2020-05-21 16:55:56 +0100

    Update thumbnail maker so it doesn't recreate existing files.

--- make_thumbnails.py
+++ make_thumbnails.py
@@ -7,10 +7,14 @@ JMG
 
 from PIL import Image
 import glob
+from pathlib import Path
 
 for filename in glob.glob('*.jpg'):
     if 'thumb' in filename:
         continue
+    thumb_filename = filename[:-4] + '_thumb.jpg'
+    if Path(thumb_filename).exists():
+        continue
     im = Image.open(filename)
     im.thumbnail((256, 256), Image.ANTIALIAS)
-    im.save(filename[:-4] + '_thumb.jpg', 'JPEG')
+    im.save(thumb_filename, 'JPEG')
